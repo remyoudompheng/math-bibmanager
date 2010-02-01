b529919b866c31f3f629e805bc87ce3f21deae12
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-02-02 00:42:38 +0100

    New files for main_window class.

---
+++ src/MainWindow.hpp
@@ -0,0 +1,43 @@
+/*
+ * math-bibmanager
+ * A Bibliography manager for mathematicians
+ *
+ * Copyright (C) 2010 Rémy Oudompheng
+
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
+
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
+#include <gtkmm.h>
+
+class MainWindow : public Gtk::Window
+{
+public:
+  MainWindow(BaseObjectType* cobject, const Glib::RefPtr<Gtk::Builder>& refGlade);
+  virtual ~MainWindow();
+
+protected:
+  Glib::RefPtr<Gtk::Builder> uidef;
+
+  void _on_quit_activate();
+};
+
+#endif //!MAIN_WINDOW_H       
