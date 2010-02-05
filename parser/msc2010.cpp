c28683d500d150d9245069bd0213632feb1376f0
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-02-05 14:21:30 +0100

    MSC2010 major categories.

--- parser/msc2010.cpp
+++ parser/msc2010.cpp
@@ -37,3 +37,74 @@ MSC2010Entry::MSC2010Entry(string str)
   stringstream s (str.substr(0,2) + " " + str[2] + " " + str.substr(3,2));
   s >> maj >> med >> min;
 }
+
+strign MSC2010Entry::print_major()
+{
+  switch(maj)
+    {
+    case 00: return "General";
+    case 01: return "History and biography";
+    case 03: return "Mathematical logic and foundations";
+    case 05: return "Combinatorics";
+    case 06: return "Order, lattices, ordered algebraic structures";
+    case 08: return "General algebraic systems";
+    case 11: return "Number theory";
+    case 12: return "Field theory and polynomials";
+    case 13: return "Commutative algebra";
+    case 14: return "Algebraic geometry";
+    case 15: return "Linear and multilinear algebra; matrix theory";
+    case 16: return "Associative rings and algebras";
+    case 17: return "Nonassociative rings and algebras";
+    case 18: return "Category theory; homological algebra";
+    case 19: return "K-theory";
+    case 20: return "Group theory and generalizations";
+    case 22: return "Topological groups, Lie groups";
+    case 26: return "Real functions";
+    case 28: return "Measure and integration";
+    case 30: return "Functions of a complex variable";
+    case 31: return "Potential theory";
+    case 32: return "Several complex variables and analytic spaces";
+    case 33: return "Special functions (33 deals with the properties of functions as functions)";
+    case 34: return "Ordinary differential equations";
+    case 35: return "Partial differential equations";
+    case 37: return "Dynamical systems and ergodic theory";
+    case 39: return "Difference and functional equations";
+    case 40: return "Sequences, series, summability";
+    case 41: return "Approximations and expansions";
+    case 42: return "Harmonic analysis on Euclidean spaces";
+    case 43: return "Abstract harmonic analysis";
+    case 44: return "Integral transforms, operational calculus";
+    case 45: return "Integral equations";
+    case 46: return "Functional analysis";
+    case 47: return "Operator theory";
+    case 49: return "Calculus of variations and optimal control; optimization";
+    case 51: return "Geometry";
+    case 52: return "Convex and discrete geometry";
+    case 53: return "Differential geometry";
+    case 54: return "General topology";
+    case 55: return "Algebraic topology";
+    case 57: return "Manifolds and cell complexes";
+    case 58: return "Global analysis, analysis on manifolds";
+    case 60: return "Probability theory and stochastic processes";
+    case 62: return "Statistics";
+    case 65: return "Numerical analysis";
+    case 68: return "Computer science";
+    case 70: return "Mechanics of particles and systems";
+    case 74: return "Mechanics of deformable solids";
+    case 76: return "Fluid mechanics";
+    case 78: return "Optics, electromagnetic theory";
+    case 80: return "Classical thermodynamics, heat transfer";
+    case 81: return "Quantum theory";
+    case 82: return "Statistical mechanics, structure of matter";
+    case 83: return "Relativity and gravitational theory";
+    case 85: return "Astronomy and astrophysics";
+    case 86: return "Geophysics";
+    case 90: return "Operations research, mathematical programming";
+    case 91: return "Game theory, economics, social and behavioral sciences";
+    case 92: return "Biology and other natural sciences";
+    case 93: return "Systems theory; control";
+    case 94: return "Information and communication, circuits";
+    case 97: return "Mathematics education";
+    default: return "Category not implemented";
+    }
+}
