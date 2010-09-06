e7a8fcf074858f0887c3454f3f1d72cbe508a32c
Rémy Oudompheng <oudomphe@math.unice.fr>
2010-09-06 22:02:30 +0200

    Add support for building with CMake.

---
+++ config.h.cmake
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define PACKAGE_NAME "Mathkaba"
+#define PACKAGE_VERSION "${MATHKABA_VERSION}"
+#define PACKAGE_STRING "Mathkaba ${MATHKABA_VERSION}"
+#define PACKAGE_AUTHORS "Rémy Oudompheng"
+#define PACKAGE_COPYRIGHT "Copyright 2010, Rémy Oudompheng"
+
+#endif //! CONFIG_H
